f2a99d3e045bca0b927d71d240671ace51e6cdb8
rau1avi1a <avila0118@icloud.com>
2023-08-05 20:41:03 -0700

    initial deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -22,10 +22,6 @@ module.exports = () => {
         template: './index.html',
         title: 'J.A.T.E'
       }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'src-sw.js',
-      }),
       new WebpackPwaManifest({
         name: 'J.A.T.E',
         short_name: 'J.A.T.E',
@@ -34,15 +30,18 @@ module.exports = () => {
         theme_color: '#225ca3',
         start_url: './',
         publicPath: './',
-        // icons: [
-        //   {
-        //     src: path.resolve('src/images/logo.png'),
-        //     sizes: [96, 128, 192, 256, 384, 512],
-        //     destination: path.join('assets', 'icons'),
-        //   },
-        // ],
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
-
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
     ],
 
     module: {
