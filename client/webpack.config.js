b11c1d5fea47696431b6b96fcda93ed4e0ab0a36
rau1avi1a <avila0118@icloud.com>
2023-08-05 20:22:56 -0700

    initial deploy

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -34,13 +34,13 @@ module.exports = () => {
         theme_color: '#225ca3',
         start_url: './',
         publicPath: './',
-        icons: [
-          {
-            src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
+        // icons: [
+        //   {
+        //     src: path.resolve('src/images/logo.png'),
+        //     sizes: [96, 128, 192, 256, 384, 512],
+        //     destination: path.join('assets', 'icons'),
+        //   },
+        // ],
       }),
 
     ],
